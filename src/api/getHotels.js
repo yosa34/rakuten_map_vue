245756474d7c6d31b07661dfb73f2be6bbdff98f
JunzoMatsunoo <junzo.matsunoo@wireframes.co.jp>
2020-07-30 12:37:54 +0900

    getHotelsのAPIを実装 main.jsにてテスト用の記述を追記

--- src/api/getHotels.js
+++ src/api/getHotels.js
@@ -1,4 +1,17 @@
+import axios from 'axios'
 /**
  * ホテルの取得APIに接続するメソッドを定義する
  * ホテルへの接続は、axiosを使って実装する
  */
+export default function (params = {}) {
+    axios.get(
+        'https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426',
+        {
+            params: {
+                ...params,
+                datumType: 1,
+                applicationId: process.env.VUE_APP_APP_ID
+            }
+        }
+        )
+}
--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
 
+import getHotels from './api/getHotels'
+
+window.getHotels = getHotels
+window.hotelOptions = {
+  checkinDate: '2020-12-01',
+  checkoutDate: '2020-12-02',
+  latitude: 34.7024898,
+  longitude: 135.4937619,
+  adultNum: 3,
+}
+
 Vue.config.productionTip = false
 
 new Vue({
